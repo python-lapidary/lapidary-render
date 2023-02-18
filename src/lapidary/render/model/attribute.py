31f3ce318b751a1c61dc3c333052e37cc20dc2b9
Raphael Krupinski <rafalkrupinski@users.noreply.github.com>
2023-02-18 23:57:25 +0100

    🐛 Fix handling enum keys.

--- src/lapidary/render/model/attribute.py
+++ src/lapidary/render/model/attribute.py
@@ -4,7 +4,7 @@ from typing import Any, Optional, Union
 from lapidary.runtime import openapi
 from lapidary.runtime.model import ResolverFunc, from_type
 from lapidary.runtime.module_path import ModulePath
-from lapidary.runtime.names import check_name, maybe_mangle_name
+from lapidary.runtime.names import check_name, maybe_mangle_name, get_enum_field_name
 
 from .attribute_annotation import AttributeAnnotationModel, get_attr_annotation
 
@@ -57,12 +57,11 @@ def get_attribute(
     )
 
 
-def get_enum_attribute(value: Any, name: str) -> AttributeModel:
-    name = maybe_mangle_name(name, False)
-    check_name(name, False)
-    value = "'" + value.replace("'", r"\'") + "'" if value is not None else None
+def get_enum_attribute(value: Any, name: Optional[str]) -> AttributeModel:
+    if isinstance(value, str):
+        value = "'" + value.replace("'", r"\'") + "'" if value is not None else None
     return AttributeModel(
-        name=name,
+        name=maybe_mangle_name(name, False) if name else get_enum_field_name(value),
         annotation=AttributeAnnotationModel(
             type=from_type(type(value)),
             field_props={'default': value},
