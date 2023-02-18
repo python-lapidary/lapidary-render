f02652db8325c16b2d29d12cd04286e6157c8d58
Raphael Krupinski <rafalkrupinski@users.noreply.github.com>
2023-02-19 00:15:42 +0100

    🐛 Fix template helper.

--- src/lapidary/render/jinja.py
+++ src/lapidary/render/jinja.py
@@ -1,18 +1,17 @@
 from collections.abc import Mapping
 from functools import cached_property
+from importlib.metadata import version as get_version
 from pathlib import Path
 from typing import Any
 
 import jinja2.ext
 from lapidary.runtime import openapi
 from lapidary.runtime.model import TypeHint, ResolverFunc, get_resolver, resolve_ref
-from lapidary.runtime.model.refs import _schema_get
 from lapidary.runtime.model.type_hint import BUILTINS
 from lapidary.runtime.module_path import ModulePath
 
-from lapidary.render.model import get_client_class_module, AuthModule, get_auth_module
-from lapidary.render.model.schema_module import get_schema_module
-from importlib.metadata import version as get_version
+from .model import get_client_class_module, AuthModule, get_auth_module
+from .model.schema_module import get_schema_module, get_param_model_classes_module
 
 document: Mapping[str: Any]
 package: str
@@ -41,6 +40,8 @@ class LapidaryModel:
         return str(ModulePath.from_reference(self._root, path, self.model).to_path(Path('.'), False))
 
     def type_hint(self, type_ref: TypeHint | str, this_mod: ModulePath | str) -> str:
+        if type_ref is None:
+            return "None"
         if isinstance(type_ref, str):
             import builtins
             if type_ref in dir(builtins):
@@ -48,10 +49,10 @@ class LapidaryModel:
             else:
                 type_ref = TypeHint.from_str(type_ref)
         if isinstance(this_mod, str) and this_mod.startswith("#"):
-            _, this_mod, _ = self._resolver(openapi.Reference(ref=this_mod), openapi.Schema)
+            _, this_mod, _ = self._resolver(openapi.Reference(ref=this_mod))
         if isinstance(this_mod, ModulePath):
             this_mod = str(this_mod)
-        return type_ref.str(this_mod)
+        return type_ref.to_str(this_mod)
 
     @property
     def _root(self) -> ModulePath:
@@ -66,8 +67,11 @@ class LapidaryModel:
         return get_client_class_module(model, self._root / "client", self._root, self._resolver)
 
     def schema_model(self, path: str):
+        if path.startswith("#/paths/") and path.endswith("/parameters"):
+            op = resolve_ref(self.model, path[:-11], openapi.Operation)
+            return get_param_model_classes_module(op, ModulePath.from_reference(self._root, path, self.model), self._resolver)
         return get_schema_module(
-            _schema_get(self.model, path, openapi.Schema),
+            resolve_ref(self.model, path),
             path.split('/')[-1],
             ModulePath.from_reference(self._root, path, self.model),
             self._resolver,
