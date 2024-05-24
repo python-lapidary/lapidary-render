284ca1bb15b15784a316f258b8576dbf65906ffb
Raphael Krupinski <3732079+rafalkrupinski@users.noreply.github.com>
2024-05-24 23:38:55 +0200

    ✅ Add property schema test.

--- tests/process/test_schema.py
+++ tests/process/test_schema.py
@@ -3,7 +3,9 @@ from collections.abc import Mapping
 from pathlib import Path
 
 import pytest
+import ruamel.yaml
 
+from lapidary.render import json_pointer
 from lapidary.render.model import OpenApi30Converter, openapi, python, stack
 from lapidary.runtime.http_consts import MIME_JSON
 
@@ -13,8 +15,15 @@ logging.getLogger('lapidary').setLevel(logging.DEBUG)
 
 @pytest.fixture
 def document() -> openapi.OpenApiModel:
-    doc_text = (Path(__file__).parent.parent / 'petstore.json').read_text()
+    doc_text = (Path(__file__).parent.parent / 'e2e/init/petstore/petstore.json').read_text()
     return openapi.OpenApiModel.model_validate_json(doc_text)
+
+
+@pytest.fixture
+def doc_dummy() -> openapi.OpenApiModel:
+    doc_text = (Path(__file__).parent.parent / 'e2e/init/dummy/dummy.yaml').read_text()
+    yaml = ruamel.yaml.YAML(typ='safe')
+    return openapi.OpenApiModel.model_validate(yaml.load(doc_text))
 
 
 def test_schema_str(document: openapi.OpenApiModel) -> None:
@@ -41,3 +50,17 @@ def test_schema_array(document: openapi.OpenApiModel) -> None:
         module='builtins', name='list', args=(python.TypeHint.from_str('petstore.components.schemas.User.schema:User'),)
     )
     assert converter.schema_converter.schema_modules[0].body[0].class_name == 'User'
+
+
+def test_property_schema(doc_dummy: openapi.OpenApiModel) -> None:
+    converter = OpenApi30Converter(python.ModulePath('dummy'), doc_dummy)
+    operations: Mapping[str, openapi.Operation] = doc_dummy.paths.paths['/test/'].model_extra
+
+    schema = converter.schema_converter.process_schema(
+        operations['get'].parameters[1].schema_,
+        stack.Stack(('#', 'paths', json_pointer.encode_json_pointer('/test/'), 'get', 'parameters', '1', 'schema')),
+    )
+
+    assert schema == python.TypeHint.from_str(
+        'dummy.paths.u_00007e01testu_00007e01.get.parameters.u_000031.schema.schema:schema'
+    )
