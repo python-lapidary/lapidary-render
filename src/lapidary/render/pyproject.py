220b1dca559546edc97f09ed808bc90956a8c5eb
Rafał Krupiński <rafal@lapidary.dev>
2025-12-06 19:11:11 +0100

    🎨 Sort dictionaries.

--- src/lapidary/render/pyproject.py
+++ src/lapidary/render/pyproject.py
@@ -8,15 +8,15 @@ def mk_pyproject_toml(
     return {
         'build-system': {'build-backend': 'poetry.core.masonry.api', 'requires': ['poetry-core>=2']},
         'project': {
-            'name': config.package,
-            'description': f'Client library for {title}',
-            'version': '0.1.0',
             'authors': [],
-            'license': '',
-            'requires-python': '~=3.9',
             'dependencies': [
                 'lapidary~=0.12.0',
             ],
+            'description': f'Client library for {title}',
+            'license': '',
+            'name': config.package,
+            'requires-python': '~=3.9',
+            'version': '0.1.0',
         },
         'tool': {
             'lapidary': config.model_dump(mode='json', exclude_unset=True, exclude_defaults=True),
